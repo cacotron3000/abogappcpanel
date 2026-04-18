7449490471d1c05456b1fe6279701773831213d9
cacotron3000 <carlosgorrono@gmail.com>
2026-04-18 16:25:14 -0400

    Update default admin password to R@bula04

--- backend/seed_users.sql
+++ backend/seed_users.sql
@@ -3,7 +3,7 @@
 -- jmgorrono => Alexturner505.
 -- cgorrono => Cpcgv1614.
 -- ijara => Benja1937
--- admin => Admin1234.
+-- admin => R@bula04
 --
 -- Nota: el login de la app valida formato email en frontend,
 -- por eso se usan correos del dominio gjabogados.cl.
@@ -13,7 +13,7 @@ VALUES
   ('jmgorrono@gjabogados.cl', 'jmgorrono', '$2y$12$b6cd5zkb33NHFZZHZxkzMOHAqdMKcPnmIIi91HEM.zCw7UhcdBOjO', 1, 1),
   ('cgorrono@gjabogados.cl', 'cgorrono', '$2y$12$KLgi3xubyKS2FdvUu/.dbOth23gw6yzZyCzHJvqgimkn6URUKLiUu', 1, 1),
   ('ijara@gjabogados.cl', 'ijara', '$2y$12$UEe.N7v1FwHr5TZUhJfVPOp3kuSV6SAnukpoJ4TwVj0qf.sk25TvS', 1, 1),
-  ('admin@gjabogados.cl', 'Administrador', '$2y$12$qnNEWqswq7XvU8lENQLKpepa7A3aELpZbzlkcuDwdkn0.KENv4rFa', 1, 1)
+  ('admin@gjabogados.cl', 'Administrador', '$2y$12$Up2MpbR5NJAYQ1.T2Nme.eXhC/bSqXR9AjSxokFdKJYFiCNOp.C1m', 1, 1)
 ON DUPLICATE KEY UPDATE
   nombre = VALUES(nombre),
   password_hash = VALUES(password_hash),
